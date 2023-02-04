9e1c39eca2d743d6bf3ffdf18dbb15268964a539
kelvinEskca <beredugokelvin@gmail.com>
2023-02-04 21:35:49 +0100

    update

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({children}) => {
     const auth = localStorage.getItem('token');
     return (
-        auth ? children : <Navigate to='/prayer' />
+        auth ? children : <Navigate to='/login' />
     );
 }
  
